6408753ab601ab7fc93be07cc1d826c6349f6de4
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-01-18 12:59:47 +0500

    add inline buttons for tg bot

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    CallbackQueryHandler,
+)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     buttons = [
         [
-            
+            InlineKeyboardButton("Toshkent", callback_data="region_1"),
+            InlineKeyboardButton("Samarqand", callback_data="region_2"),
         ]
     ]
-    
+
     user = update.message.from_user
     await update.message.reply_html(
-        "Assalomu alaykum <b>{}!</b>\n \n<b>Ramazon oyi muborak bo'lsin</b>\n \nSizga qaysi minatqa bo;yicha ma'lumot berayin?".format(
+        "Assalomu alaykum <b>{}!</b>\n \n<b>Ramazon oyi muborak bo'lsin</b>\n \nSizga qaysi minatqa bo'yicha ma'lumot berayin?".format(
             user.first_name
         ),
+        reply_markup=InlineKeyboardMarkup(buttons),
     )
+
+
+async def region_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    await query.answer()
+
+    if query.data == "region_1":
+        await query.message.reply_text("📍 Siz Toshkentni tanladingiz")
+    elif query.data == "region_2":
+        await query.message.reply_text("📍 Siz Samarqandni tanladingiz")
 
 
 def main():
@@ -25,7 +43,9 @@ def main():
         .token("8522778567:AAF6h7icUvkw4rGv0uGkaSzwAWNXRLaWWyI")
         .build()
     )
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(region_callback))
     app.run_polling()
 
 
